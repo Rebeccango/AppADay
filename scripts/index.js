962cbcbdfa4aa16d2a424824b7199a98a2e41398
Rebecca Ngo <40474210+Rebeccango@users.noreply.github.com>
2019-05-07 00:00:57 -0400

    fixed function in js for timer

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,40 @@
 //   COUNTDOWN TIMER
 
 // 1. Set valid end date
-    // need to get the amount of time user is inputting we will begin with 25 minutes
-var minutes = document.querySelector("#minutes").innerHTML;
-    seconds = document.querySelector("#seconds").innerHTML;
-
-function pomadoro(minute, seconds) {
-    pom = minute*(60000) + seconds*(1000)
-    return pom;
-}
+// need to get the amount of time user is inputting we will begin with 25 minutes
 
 var now = new Date().getTime();
+var minutes = document.querySelector("#minutes").innerHTML;
+var seconds = document.querySelector("#seconds").innerHTML;
+
 var deadline = now + pomadoro(minutes, seconds);
 var remainingTime = deadline - now;
 
-if(deadline === now) {
-    console.log("time is up");
+function pomadoro(min, sec) {
+    var pom = (min * 60000) + (sec * 1000);
+    return pom;
 }
+
+console.log(pomadoro(25, 10));
+
+function startCountdown(time) {
+    var counter = time;
+
+    var interval = setInterval(() => {
+        console.log(counter);
+        counter--;
+
+    if(counter < 0) {
+        console.log('function');
+
+        clearInterval(interval);
+        console.log('Ding!');
+        };
+    }, 1000);
+};
+
+startCountdown(seconds);
+
 // 2. Calculate time remaining 
 // 3. Convert time to a usable format 
 // 4. output the clock data as a reusable object
