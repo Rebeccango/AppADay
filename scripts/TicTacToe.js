efc6bc46b7aecdc547d078b4b50d555669e3084b
Rebecca Ngo <40474210+Rebeccango@users.noreply.github.com>
2019-05-10 15:33:46 -0400

    switch statement implmented to determine first player

--- scripts/TicTacToe.js
+++ scripts/TicTacToe.js
@@ -22,26 +22,63 @@ function userSelect(event) {
     game.user = userConfig[1].id;
     game.computer = userConfig[0].id;
     }
-    firstPlayer();
+    var userAns = prompt("Now to determine who will go first. Rock, paper, or scissors? Type your response below").charAt(0);
+    firstPlayer(userAns);
     return console.log(game);
 }
 
 // Now I need to know who will want to start, maybe implement a rock/paper scissor function 
-function firstPlayer(answer) {
-    let options = ["r", "p", "s"],
-    userAns = prompt("rock, paper, or scissors?").charAt(0),
-    cpuAns = Math.random();
+function firstPlayer(response) {
+    let options = ["rock", "paper", "scissors"],
+        cpuAns = options[Math.floor(Math.random()*Math.floor(3))];
+    
+    console.log(`itial useransr value ${response}`);
+    
+    switch(response) {
+        case 'r':
+        userAns = 'rock';
+        console.log(userAns);
+        break;
 
-    if(userAns !== ("r" || "p" || "s")) {
-        console.log("need new answer");
-        window.alert("..that's not a valid option, please select again");
-        userAns = prompt("rock, paper, or scissors?").charAt(0);
-    } else {
-      console.log("halp");
+        case 'p':
+        userAns = 'paper';
+        console.log(userAns);
+        break;
+
+        case 's':
+        userAns = 'scissors';
+        console.log(userAns);
+        break;
+
+        default: 
+        window.alert("invalid response -- need new answer");
+        userAns = prompt("Now to determine who will go first. Rock, paper, or scissors? Type your response below").charAt(0);
+        firstPlayer(userAns);
     }
-    //will need to validate the answer of the input
-    console.log('need to determine who goes first');
+
+
+//     options.forEach( option => { 
+//        if(option.charAt(0) === userAns){
+//         // userAns = option;
+//         console.log(userAns);
+//    }});
 }
+ // need to determine who won that game
+//  if(userAns === cpuAns){
+//      window.alert("It's a draw!...make function to handle this case")
+//  }
+//  else if(userAns === "r" && cpuAns === "s" ) {
+//      window.alert ("you won! you get to go first!");
+//  }
+//  else if(userAns === "p" && cpuAns === "r" ) {
+//     window.alert ("you won! you get to go first!");
+// }
+// else if(userAns === "s" && cpuAns === "p" ) {
+//     window.alert ("you won! you get to go first!");
+// }
+// else {
+//     window.alert("Sorry, you lost! Computer will go first");
+//     }
 
 
 // userConfig.forEach( button =>{if(button ===)})
